80c62427814b175202fe2d1db7a0cb9fb7a99dad
sumasrisarakadam <143475880+sumasrisarakadam@users.noreply.github.com>
2023-09-14 15:36:32 +0530

    Create DefaultDemo.java

---
+++ DefaultDemo.java
@@ -0,0 +1,11 @@
+class Sample  {
+    void display() {
+        System.out.println("Hello Sasi");
+    }
+}
+public class DefaultDemo {
+    public static void main(String args[]) {
+        Sample s=new Sample();
+        s.display();
+    }
+}
