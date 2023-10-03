d1352cf7c9955ed01e5c23aa3aa0c04aabc9b72d
sumasrisarakadam <143475880+sumasrisarakadam@users.noreply.github.com>
2023-10-03 09:45:04 +0530

    Create ThisDemo2.java

---
+++ ThisDemo2.java
@@ -0,0 +1,32 @@
+//java program to demonstrate the importance of using the keyword
+//3-10-23
+//s.suma
+//ThisDemo1.java
+import java.io.*;
+class Student {
+    int rollno;
+    String name;
+    float fee;//local variable
+    Student(int rollno,String name,float fee)//parameterized contructor
+    {
+        this.rollno=rollno;//local variables
+       this.name=name;//local variables
+        this.fee=fee;//local variables
+    }
+    void display() {
+         System.out.println(rollno+" "+name+" "+fee);
+         System.out.println("Student details are ");
+    }
+}
+public class ThisDemo1{
+    public static void main(String args[]) {
+        Student s1 = new Student(4250,"suma",45700);
+        Student s2 = new Student(4234,"sushma",45700);
+        Student s3 = new Student(4252,"rekha",12000);
+        Student s4 = new Student(4241,"padhu",130000);
+        s1.display();
+         s2.display();
+          s3.display();
+           s4.display();
+}
+}
