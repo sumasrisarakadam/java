47fe0c321aef527e4020ddb9b9a9e3dd6271f4b1
sumasrisarakadam <143475880+sumasrisarakadam@users.noreply.github.com>
2023-10-04 09:53:23 +0530

    Create StaticBlockA3.java

---
+++ StaticBlockA3.java
@@ -0,0 +1,10 @@
+//java program to demonstrate without using main method
+//3-10-23
+//suma
+//StaticBlockA3.java
+public class StaticBlockA3 {
+   static {
+       System.out.println("static block is invoked");
+  System.exit(0);
+    }
+}
