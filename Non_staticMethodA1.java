fa6645183662b5518410f92f35b658f13520f1cc
sumasrisarakadam <143475880+sumasrisarakadam@users.noreply.github.com>
2023-10-04 09:31:59 +0530

    Create Non_staticMethodA1.java

---
+++ Non_staticMethodA1.java
@@ -0,0 +1,10 @@
+//java program to demonstrate static method
+//3-10-23
+//suma
+//
+public class A1 {
+    int a=40;//non-static variable
+    public static void main(String args[]) {
+        System.out.println("value os a is "+a);
+    }
+}
