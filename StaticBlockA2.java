27dbdfc8612d2c5442e33cf6e929858a197146bc
sumasrisarakadam <143475880+sumasrisarakadam@users.noreply.github.com>
2023-10-04 09:35:44 +0530

    Create StaticBlockA2.java

---
+++ StaticBlockA2.java
@@ -0,0 +1,12 @@
+//java program to demonstrate static method
+//3-10-23
+//suma
+//StaticBlockA2.java
+public class StaticBlockA2 {
+   static {
+       System.out.println("inside static block ");
+   }
+    public static void main(String args[]) {
+        System.out.println("inside the main method");
+    }
+}
